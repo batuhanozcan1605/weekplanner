46060936c516d3b7d395fb2df6f0e82f6f2d3f42
Batuhan\batuhanozcan1605 <93476021+batuhanozcan1605@users.noreply.github.com>
2024-01-05 23:36:31 +0300

    inkwell buttons on mainscreen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weekplanner/constants.dart';
 import 'package:weekplanner/widgets/schedule_view.dart';
 import 'package:weekplanner/widgets/week_view.dart';
-import '../provider/appointment_provider.dart';
 import '../widgets/daily_view.dart';
 import 'event_editing_page.dart';
 
@@ -29,57 +27,63 @@ class _MainScreenState extends State<MainScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                        onPressed: () => setState(() {
-                          scheduleView = true;
-                          weekView = false;
-                          dayView = false;
-                        }),
-                        icon: Icon(Icons.view_timeline, color: scheduleView == true ? Constants.themePurple : Colors.white)),
-                    Text('SCHEDULE', style: TextStyle(color: scheduleView == true ? Constants.themePurple : Colors.white),),
-                  ],
+                InkWell(
+                  onTap: () => setState(() {
+                    scheduleView = true;
+                    weekView = false;
+                    dayView = false;
+                  }),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.view_timeline, color: scheduleView == true ? Constants.themePurple : Colors.white),
+                      const SizedBox(height: 10,),
+                      Text('SCHEDULE', style: TextStyle(color: scheduleView == true ? Constants.themePurple : Colors.white),),
+                    ],
+                  ),
                 ),
-                SizedBox(width: 20,),
-                Column(
-                  children: [
-                    IconButton(
-                        onPressed: () => setState(() {
-                          weekView = true;
-                          scheduleView = false;
-                          dayView = false;
-                        }),
-                        icon: Icon(Icons.view_week_rounded, color: weekView == true ? Constants.themePurple : Colors.white)),
-                    Text('WEEKS', style: TextStyle(color: weekView == true ? Constants.themePurple : Colors.white),),
-                  ],
+                const SizedBox(width: 20,),
+                InkWell(
+                  onTap: () => setState(() {
+                    scheduleView = false;
+                    weekView = true;
+                    dayView = false;
+                  }),
+                  child: Column(
+                    children: [
+                    Icon(Icons.view_week_rounded, color: weekView == true ? Constants.themePurple : Colors.white),
+                      const SizedBox(height: 10,),
+                      Text('WEEKS', style: TextStyle(color: weekView == true ? Constants.themePurple : Colors.white),),
+                    ],
+                  ),
                 ),
-                SizedBox(width: 20,),
-                Column(
-                  children: [
-                    IconButton(
-                        onPressed: () => setState(() {
-                          scheduleView = false;
-                          weekView = false;
-                          dayView = true;
-                        }),
-                        icon: Icon(Icons.view_day, color: dayView == true ? Constants.themePurple : Colors.white)),
-                    Text('DAYS', style: TextStyle(color: dayView == true ? Constants.themePurple : Colors.white),),
-                  ],
+                const SizedBox(width: 20,),
+                InkWell(
+                  onTap: () => setState(() {
+                    scheduleView = false;
+                    weekView = false;
+                    dayView = true;
+                  }),
+                  child: Column(
+                    children: [
+                      Icon(Icons.view_day, color: dayView == true ? Constants.themePurple : Colors.white),
+                      const SizedBox(height: 10,),
+                      Text('DAYS', style: TextStyle(color: dayView == true ? Constants.themePurple : Colors.white),),
+                    ],
+                  ),
                 ),
               ],
             ),
             Expanded(
                 child: AnimatedSwitcher(
-                  child: weekView ? WeekView() : AnimatedSwitcher(
-                      switchInCurve: Curves.easeIn,
-                      switchOutCurve: Curves.easeOut,
-                      duration: Duration(milliseconds: 500),
-                      child: scheduleView == false ? DailyView() : const ScheduleView()),
                   switchInCurve: Curves.easeIn,
                   switchOutCurve: Curves.easeOut,
-                  duration: Duration(milliseconds: 500),)
+                  duration: const Duration(milliseconds: 500),
+                  child: weekView ? const WeekView() : AnimatedSwitcher(
+                      switchInCurve: Curves.easeIn,
+                      switchOutCurve: Curves.easeOut,
+                      duration: const Duration(milliseconds: 500),
+                      child: scheduleView == false ? const DailyView() : const ScheduleView()),)
             ),
           ],
         ),
@@ -91,13 +95,13 @@ class _MainScreenState extends State<MainScreen> {
           final String? selectedDateTimeString = prefs.getString('selectedDateTime');
           if(selectedDateTimeString != null) {
             final DateTime cellDate = DateTime.parse(selectedDateTimeString);
-            print("CELL DATE $cellDate");
+          // ignore: use_build_context_synchronously
           Navigator.of(context).push(
           MaterialPageRoute(builder: (BuildContext context) => EventEditingPage(cellDate: cellDate,)),
         );
           }
         },
-        child: Icon(Icons.add, color: Colors.black),
+        child: const Icon(Icons.add, color: Colors.black),
       ),
     );
   }
---
+++ lib/screens/pie_chart_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PieChartScreen extends StatelessWidget {
+  const PieChartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
