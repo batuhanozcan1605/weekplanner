abcef743e6f546eb11ec62f60b42e22ef21d183a
Batuhan\batuhanozcan1605 <93476021+batuhanozcan1605@users.noreply.github.com>
2024-01-07 16:38:01 +0300

    latest listview oldu

--- lib/widgets/choose_event_widget.dart
+++ lib/widgets/choose_event_widget.dart
@@ -3,15 +3,15 @@ import 'package:provider/provider.dart';
 import 'package:weekplanner/constants.dart';
 import 'package:weekplanner/data.dart';
 import 'package:weekplanner/provider/appointment_provider.dart';
-import 'package:weekplanner/screens/event_editing_page.dart';
-import 'package:weekplanner/screens/event_viewing_page.dart';
 
 class ChooseEvent extends StatelessWidget {
   const ChooseEvent({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    var screenSize = MediaQuery.of(context).size;
+    final width = screenSize.width;
+    final cardSize = (width-40)/4;
     List eventTemplate = Data().eventTemplates;
     final provider = Provider.of<AppointmentProvider>(context, listen: false);
     List latestEvents = provider.get4LatestEvents();
@@ -28,31 +28,32 @@ class ChooseEvent extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 18.0, bottom: 8),
-              child: const Text("Latest", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Segoe UI'),),
+            const Padding(
+              padding: EdgeInsets.only(left: 18.0, bottom: 8),
+              child: Text("Latest", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Segoe UI'),),
             ),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
+              SizedBox(
+                height: cardSize,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: latestEvents.length, // Adjust the number of items as needed
+                  itemBuilder: (context, index) {
+                    final event = latestEvents[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context, event);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 6.0), // Add spacing between items
+                        child: Constants().eventCard(event.subject, event.icon, event.color.withOpacity(0.7)),
+                      ),
+                    );
+                  },
+                ),
               ),
-              itemCount: latestEvents.length, // Adjust the number of items as needed
-              itemBuilder: (context, index) {
-                final event = latestEvents[index];
-                return GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context, event);
-
-                    },
-                    child: Constants().eventCard(event.subject, event.icon, event.color.withOpacity(0.7)));
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
+            const Padding(
+              padding: EdgeInsets.all(18.0),
               child: Divider(),
             ),
 
