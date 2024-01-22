503632837285a7d62221199a3629c6004ef1f57e
Batuhan\batuhanozcan1605 <93476021+batuhanozcan1605@users.noreply.github.com>
2024-01-22 21:20:04 +0300

    event viewing ad

--- lib/screens/event_viewing_page.dart
+++ lib/screens/event_viewing_page.dart
@@ -1,117 +1,171 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:weekplanner/simple_widgets.dart';
 import 'package:weekplanner/screens/event_editing_page.dart';
 import 'package:weekplanner/utils.dart';
+import '../ad_helper.dart';
 import '../model/MyAppointment.dart';
 import '../provider/appointment_provider.dart';
 
-class EventViewingPage extends StatelessWidget {
+class EventViewingPage extends StatefulWidget {
   const EventViewingPage({super.key, required this.appointment});
 
   final MyAppointment appointment;
+
+  @override
+  State<EventViewingPage> createState() => _EventViewingPageState();
+}
+
+class _EventViewingPageState extends State<EventViewingPage> {
+  BannerAd? _bannerAd;
+
+  void _createBannerAd() {
+    _bannerAd = BannerAd(
+      size: AdSize.fullBanner,
+      adUnitId: AdHelper.bannerAdUnitId,
+      listener: AdHelper.bannerAdListener,
+      request: const AdRequest(),
+    )..load();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _createBannerAd();
+  }
 
   @override
   Widget build(BuildContext context) {
     final icons = Provider.of<AppointmentProvider>(context).icons;
     ColorScheme colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: colorScheme.background,
-        title:  Text(appointment.subject,style: const TextStyle(
-            fontFamily: 'Segoe UI',
-            fontSize: 20,
-            fontWeight: FontWeight.bold
-        ),),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => EventEditingPage(appointment: appointment, iconFromEdit: icons[appointment.id])));
-            },
-            icon: const Icon(Icons.edit), color: colorScheme.onBackground,),
-          IconButton(
-            onPressed: () {
-              final provider = Provider.of<AppointmentProvider>(context, listen: false);
-              provider.deleteEvent(appointment);
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.delete), color: colorScheme.onBackground,)
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(38.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 1,
-                    child: Text('FROM', style: TextStyle(color: colorScheme.primary),)),
-                Expanded(
-                  flex: 2,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(Utils.toDate(appointment.startTime)),
-                      Text(Utils.toTime(appointment.startTime)),
-                    ],
-                  ),
-                )
-              ],
+        appBar: AppBar(
+          backgroundColor: colorScheme.background,
+          title: Text(
+            widget.appointment.subject,
+            style: const TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => EventEditingPage(
+                        appointment: widget.appointment,
+                        iconFromEdit: icons[widget.appointment.id])));
+              },
+              icon: const Icon(Icons.edit),
+              color: colorScheme.onBackground,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 18.0),
-              child: Row(
+            IconButton(
+              onPressed: () {
+                final provider =
+                    Provider.of<AppointmentProvider>(context, listen: false);
+                provider.deleteEvent(widget.appointment);
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.delete),
+              color: colorScheme.onBackground,
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(38.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Expanded(
                       flex: 1,
-                      child: Text('TO', style: TextStyle(color: colorScheme.primary))),
+                      child: Text(
+                        'FROM',
+                        style: TextStyle(color: colorScheme.primary),
+                      )),
                   Expanded(
                     flex: 2,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(Utils.toDate(appointment.endTime)),
-                        Text(Utils.toTime(appointment.endTime)),
+                        Text(Utils.toDate(widget.appointment.startTime)),
+                        Text(Utils.toTime(widget.appointment.startTime)),
                       ],
                     ),
                   )
                 ],
               ),
-            ),
-            appointment.recurrenceRule != null ? const Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-              Icon(Icons.repeat, color: SimpleWidgets.themePurple,),
-              SizedBox(width: 8,),
-              Text('Recurring Event'),
-            ],
-            ) : const Center(),
-            const Padding(
-              padding: EdgeInsets.all(18.0),
-              child: Divider(color: SimpleWidgets.themePurple,),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 18.0),
-              child: appointment.notes == null ? const Text("") : Text(appointment.notes!, style: TextStyle(color: colorScheme.onBackground),),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: 300,
-                height: 300,
-                color: Colors.white,
-                child: const Center(child: Text('AD BANNER', style: TextStyle(color: Colors.orangeAccent),)),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Text('TO',
+                            style: TextStyle(color: colorScheme.primary))),
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(Utils.toDate(widget.appointment.endTime)),
+                          Text(Utils.toTime(widget.appointment.endTime)),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+              widget.appointment.recurrenceRule != null
+                  ? const Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(
+                          Icons.repeat,
+                          color: SimpleWidgets.themePurple,
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text('Recurring Event'),
+                      ],
+                    )
+                  : const Center(),
+              const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Divider(
+                  color: SimpleWidgets.themePurple,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 18.0),
+                child: widget.appointment.notes == null
+                    ? const Text("")
+                    : Text(
+                        widget.appointment.notes!,
+                        style: TextStyle(color: colorScheme.onBackground),
+                      ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: _bannerAd == null
+                      ? Container()
+                      : SizedBox(
+                          height: 250,
+                          width: 300,
+                          child: AdWidget(ad: _bannerAd!),
+                        ),
+                ),
+              )
+            ],
+          ),
         ),
-      )
     );
   }
 }
